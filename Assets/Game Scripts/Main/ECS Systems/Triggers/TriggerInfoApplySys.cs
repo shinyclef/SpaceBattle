878030e42f7bfa798c536ffe74503fd0b4cd2440
shinyclef <shinyclef@hotmail.com>
2019-05-17 18:21:00 +1000

    Updated all packages
    All packaged updated. Entities p32. This state doesn't run without error.

--- Assets/Game Scripts/Main/ECS Systems/Triggers/TriggerInfoApplySys.cs
+++ Assets/Game Scripts/Main/ECS Systems/Triggers/TriggerInfoApplySys.cs
@@ -47,6 +47,12 @@ public class TriggerInfoApplySys : JobComponentSystem
             {
                 do
                 {
+                    // TODO Remove this size increase when unity does it on their side (preview 32?)
+                    //if (buffer.Length == buffer.Capacity)
+                    //{
+                    //    buffer.ResizeUninitialized(buffer.Capacity + 2);
+                    //}
+
                     buffer.Add(info);
                 }
                 while (TriggerMap.TryGetNextValue(out info, ref iterator));
